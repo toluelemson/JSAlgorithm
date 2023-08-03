1166b8d79fd899d329064d9630f8c565496cfb25
toluwani <toluwani@ut.ee>
2023-08-03 19:28:32 +0300

    main

---
+++ src/main.ts
@@ -0,0 +1,118 @@
+//import { validTime } from './validTime/validTime';
+// import { adjacentElementsProduct } from './adjacentElementsProduct/adjacentElementsProduct';
+// import {findSecondLongestString} from "./secondLongestString/secondLongestString";
+// import {uniqueDigitProducts} from "./uniqueDigitProducts/uniqueDigitsProducts";
+// import {mostFrequentDigitSum} from "./mostFrequentDigitSum/mostFrequentDigitSum";
+// import {missingLetters} from "./missingLetters/missingLetters";
+// import {convertStringToArrayAndBack} from "./practice/CharAt/convertStringToArrayAndBack";
+// import {makeArrayConsecutive2} from "./makeArrayConsecutive2/makeArrayConsecutive2";
+// import {longestDigitsPrefix} from "./longestDigitsPrefix/longestDigitsPrefix";
+// import {isLucky} from "./isLucky/isLucky";
+// import {integerToStringOfFixedWidth} from "./integerToStringOfFixedWidth/integerToStringOfFixedWidth";
+// import {findClosestPair} from "./findClosestPair/findClosestPair";
+// import {firstDigit} from "./firstDigit/firstDigit";
+// import {firstDuplicate} from "./firstDuplicate/firstDuplicate";
+// import {firstNotRepeatingCharacter} from "./firstNotRepeatingCharacter/firstNotRepeatingCharacter";
+// import {flattenArray} from "./flattenArray/flattenArray";
+// import {growingPlant} from "./growingPlant/growingPlant";
+// import {houseNumbersSum} from "./houseNumbersSum/houseNumbersSum";
+// import {houseOfCats} from "./houseOfCats/houseOfCats";
+// import {htmlEndTagByStartTag} from "./htmlEndTagByStartTag/htmlEndTagByStartTag";
+// import {incorrectPasscodeAttempts} from "./incorrectPasswordAttempts/incorrectPasswordAttempts";
+// import {incrementalBackups} from "./incrementalBackups/incrementalBackups";
+// import {fermactor} from "./fermactor/fermactor";
+// import {fareEstimator} from "./fareEstimator/fareEstimator";
+// import {extractMatrixColumn} from "./extractMatrixColumn/extractMatrixColumn";
+// import {evenDigitsOnly} from "./evenDigitsOnly/evenDigitsOnly";
+// import {arrayMaxConsecutiveSum} from "./arrayMaxConsecutiveSum/arrayMaxConsecutiveSum";
+// import {arrayMaximalAdjacentDifference} from "./arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference";
+// import {arrayPreviousLess} from "./arrayPreviousLess/arrayPreviousLess";
+// import {WiseHackerRank} from "./practice/practice";
+// import {candies} from "./candies/candies";
+// import {centuryFromYear} from "./centuryFromYear/centuryFromYear";
+// import {chessBoardCellColor} from "./chessBoardCellColor/chessBoardCellColor";
+// import {chunkyMonkey} from "./chunkyMonkey/chunkyMonkey";
+// import {commonCharacterCount} from "./commonCharacterCount/commonCharacterCount";
+//import {alphabetSubsequence} from "./alphabetSubSequence/alphabetSubSequence";
+//import {alternatingSums} from "./alternatingSums/alternatingSums";
+//import {areEquallyStrong} from "./areEquallyStrong/areEquallyStrong";
+import {areSimilar} from "./areSimilar/areSimilar";
+
+
+
+function main () {
+    // console.log(validTime('13:58'));
+    // console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+    // const inputArray = ['apple', 'dragonfruit', 'banana', 'coconut', 'potatopotato', 'fffffedfhjdfbejfbejejeeeee'];
+    // console.log(findSecondLongestString(inputArray)); // Output: 'coconut'
+
+    //console.log(uniqueDigitProducts([2, 8, 121, 42, 222, 23]));
+    // console.log(mostFrequentDigitSum(88));
+    // console.log(missingLetters("abcede"));
+    // console.log(convertStringToArrayAndBack('Hello'));
+    // console.log(makeArrayConsecutive2([1, 2, 9, 10, 12, 14, 19, 30]));
+    //console.log(longestDigitsPrefix('123aa1'));
+    // console.log(isLucky(1230));
+    // console.log(isLucky(239017));
+    // console.log(integerToStringOfFixedWidth(1234, 2));
+    // console.log(integerToStringOfFixedWidth(1234, 4));
+    // console.log(integerToStringOfFixedWidth(1234, 5));
+    // console.log(findClosestPair([1, 0, 2, 4, 3, 0], 5));
+    // console.log(findClosestPair([2, 3, 7], 8));
+    // console.log(firstDigit('var_1__Int'));
+    // console.log(firstDigit('q2q-q'));
+    // console.log(firstDuplicate([2, 1, 3, 5, 3, 2]));
+    // console.log(firstNotRepeatingCharacter('abacabaabacaba'));
+    // console.log(firstNotRepeatingCharacter('abacabad'));
+    // console.log(flattenArray([[["a"]], [["b"]]]));
+    // console.log(flattenArray([1, [2], [3, [[4]]]]));
+    // console.log(growingPlant(100, 10, 910))
+    //console.log(houseNumbersSum([5, 1, 2, 3, 0, 1, 5, 0, 2]));
+    // console.log(houseOfCats(6));
+    // console.log(houseOfCats(20));
+    // console.log(htmlEndTagByStartTag("<button type='button' disabled>"));
+    // console.log(htmlEndTagByStartTag('<i>'));
+    // console.log(incorrectPasscodeAttempts('1111', [ "4444", "9999", "3333", "8888", "2222", "7777", "0000", "6666", "7285", "5555"]));
+    // console.log(incorrectPasscodeAttempts('1111', ["1111", "4444", "9999", "3333", "8888", "2222", "7777", "0000", "6666", "7285", "5555", "1111"]));
+    // console.log(incrementalBackups(461620205, [[461620203, 1],
+    // [461620204, 2],
+    // [461620205, 6],
+    // [461620206, 5],
+    // [461620207, 3],
+    // [461620207, 5],
+    // [461620208, 1]]));
+    // console.log(fermactor(15));
+    // console.log(fareEstimator(30, 7, [0.2, 0.35, 0.4, 0.45], [1.1, 1.8, 2.3, 3.5]));
+    // console.log(extractMatrixColumn([[1, 1, 1, 2], [0, 5, 0, 4], [2, 1, 3, 6]], 2));
+    // console.log(evenDigitsOnly(248622));
+    // console.log(evenDigitsOnly(642386));
+    //console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
+    // console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
+    // console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
+    // const numbers = [1, 2, 3];
+    // const k = 4;
+    // console.log(WiseHackerRank(numbers, k));
+    // console.log(candies(3, 10));
+    // console.log(candies(4, 50));
+    // console.log(centuryFromYear(1905));
+    // console.log(centuryFromYear(1700));
+    // console.log(chessBoardCellColor('A1', 'C3'));
+    // console.log(chessBoardCellColor('A1', 'H3'));
+    // console.log(chunkyMonkey([0, 1, 2, 3, 4, 5], 4));
+    // console.log(commonCharacterCount('aabcc', 'adcaa'));
+    // console.log(alphabetSubsequence('zab'))
+    // console.log(alphabetSubsequence('zab'))
+    // console.log(alphabetSubsequence('effg'))
+    // console.log(alphabetSubsequence('cdce'))
+    // console.log(alphabetSubsequence('ace'))
+    // console.log(alphabetSubsequence('bxz'))
+    // console.log(alternatingSums([50, 60, 60, 45, 70]))
+    // console.log(areEquallyStrong(10, 15, 15, 10))
+    // console.log(areEquallyStrong(15, 10, 15, 10))
+    // console.log(areEquallyStrong(15, 10, 15, 9))
+    console.log(areSimilar([1, 2, 3], [1, 2, 3]));
+    console.log(areSimilar([1, 2, 3], [2, 1, 3]));
+    console.log(areSimilar([1, 2, 2], [2, 1, 1]));
+}
+
+main()
