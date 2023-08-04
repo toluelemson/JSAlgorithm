1b6d3a119b9eb8d8fd8431ccc2393dac3011c3bb
toluwani <toluwani@ut.ee>
2023-08-04 11:03:00 +0300

    arrayMaxConsecutiveSum

--- src/arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
+++ src/arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
@@ -1,16 +1,15 @@
 export function arrayMaxConsecutiveSum(inputArray: number[], k: number): number {
-
+    let sum = 0
     let max = 0
 
-    for(let i =0; i<k; i++){
-        let sum = inputArray[i] + inputArray[i + 1]
-        if (sum > max ) {
+    for(let i = 0; i<inputArray.length; i++){
+        for(let j = i; j<k; j++){
+            sum += inputArray[j]
+        }
+        if (sum > max){
             max = sum
         }
     }
-
     return max
-
 }
-
 // console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
